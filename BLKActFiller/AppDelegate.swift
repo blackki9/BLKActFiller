82839ca7f501b88ccb72d5d90e0c36fc2246b180
blackki9 <blackki9@gmail.com>
2015-06-20 16:26:13 +0300

    Initial Commit

---
+++ BLKActFiller/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BLKActFiller
+//
+//  Created by black9 on 20/06/15.
+//  Copyright © 2015 black9. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BLKActFiller/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BLKActFiller
+//
+//  Created by black9 on 20/06/15.
+//  Copyright © 2015 black9. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
